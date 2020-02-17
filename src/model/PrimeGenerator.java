c540ecefc52d605e2f45f0e3252a3f6d9c005b0e
marisolcitag <35766585+marisolcitag@users.noreply.github.com>
2020-02-16 22:42:19 -0500

    update model and ui package

--- src/model/PrimeGenerator.java
+++ src/model/PrimeGenerator.java
@@ -14,15 +14,24 @@ public class PrimeGenerator {
 	 */
 	private boolean[] primes; 
 	
-
 	/**
 	 * Description: This algorithm uses the concept of divisibility in each number of an array 
 	 * Return: An array which contains numbers primes and not.
 	 * Input: An array with natural numbers to "n" number.
 	 */
+	
+	private int columns;
+	
+	private int rows;
+	
+	
+	
 	public void firstAlgorithm() {
-		setPrimes(new boolean[top]);
-		for (int i = 0; i < primes.length; i++) {
+		primes = new boolean[top];
+		for (int i = 1; i < primes.length; i++) {
+			primes[i] = true;
+		}
+		for (int i = 1; i < primes.length; i++) {
 			int isPrime = 0;
 			for(int j= i+1; j>=1 && isPrime<=2; j--) {
 				if((i+1) % j == 0) {
@@ -30,8 +39,12 @@ public class PrimeGenerator {
 				}
 			}
 			if(isPrime>2) {
-				primes[i] = true;
+				primes[i] = false;
 			}
+		}
+		
+		for (int i = 1; i < primes.length; i++) {
+			System.out.println((i+1)+":"+ primes[i]);
 		}
 	}
 	
@@ -41,16 +54,20 @@ public class PrimeGenerator {
 	 * Input: An array with natural numbers to "n" number.
 	 */
 	public void secondAlgorithm() {
-		setPrimes(new boolean[top+1]);
-		for (int i = 2; i < primes.length; i++) {
+		primes = new boolean[top];
+		for (int i = 1; i < primes.length; i++) {
 			primes[i] = true;
-		}		
-		for (int i = 2; i <= (int)Math.sqrt(primes.length-1); i++) {
-			if(primes[i]==true) {
-				for(int j=i; j <= (int)((primes.length-1)/(i));j++) {
-					primes[i*j] = false;
+		}
+		
+		for (int i = 1; i <= (int)Math.sqrt(top); i++) {
+			if(primes[i]) {
+				for(int j=(i+1); j <= (int)((top)/(i+1));j++) {
+					primes[((i+1)*(j))-1] = false;
 				}
 			}
+		}
+		for (int i = 1; i < primes.length; i++) {
+			System.out.println((i+1)+":"+ primes[i]);
 		}
 	}
 	/**
@@ -59,43 +76,77 @@ public class PrimeGenerator {
 	 * Rights Reserved: https://www.geeksforgeeks.org/sieve-of-atkin/
 	 */
 	public void thirdAlgorithm() {
-		setPrimes(new boolean[top]);
-		for(int x=1; x*x < top; x++ ) {
-			for(int y=1; y*y < top; y++ ) {
-				
-				int n = (4 * x * x) + (y * y);
-				if(n <= top && ( n% 12 == 1 || n% 12 == 5)) {
-					primes[n] ^= true;
-				}
-				
-				n = (3 * x * x) + (y * y);
-				if(n <= top && n % 12 == 7) {
-					primes[n] ^= true;
-				}
-				
-				n = (3 * x * x) - (y * y);
-				if(n <= top && n % 12 == 11 && x > y) {
-					primes[n] ^= true;
+		
+		primes = new boolean[top+1];
+		
+		if(top==2) {
+			primes[1] = true;
+		}else if(top==3) {
+			primes[1] = true;
+			primes[2] = true;
+		}else {
+			
+			primes[1] = true;
+			primes[2] = true;
+			
+			for(int x=1; x*x < top; x++ ) {
+				for(int y=1; y*y < top; y++ ) {
+					
+					int n = (4 * x * x) + (y * y);
+					if(n <= top && ( n% 12 == 1 || n% 12 == 5)) {
+						primes[n] ^= true;
+					}
+					
+					n = (3 * x * x) + (y * y);
+					if(n <= top && n % 12 == 7) {
+						primes[n] ^= true;
+					}
+					
+					n = (3 * x * x) - (y * y);
+					if(n <= top && n % 12 == 11 && x > y) {
+						primes[n] ^= true;
+					}
 				}
 			}
-		}
-		
-		
-		for(int z=5; z*z < top; z++) {
-			if(primes[z]) {
-				for(int m= z*z; m < top; m+= z*z) {
-					primes[m] = false;
+			
+			
+			for(int z=5; z*z < top; z++) {
+				if(primes[z]) {
+					for(int m= z*z; m < top; m+= z*z) {
+						primes[m] = false;
+					}
 				}
 			}
+			
+			
 		}
 	}
+	
+	
 
 	public int getTop() {
 		return top;
 	}
-
+	
 	public void setTop(int top) {
 		this.top = top;
+		
+		int x=0;
+		int y=0;
+		int difference = top;
+		for (int i = 1; i <= top-1; i++) {
+			for (int j = 1; j <= top-1; j++) {		
+				if(i*j==(top-1)) {
+					if((i-j)<difference) {
+						x=i;
+						y=j;
+						difference = Math.abs(i-j);
+					}
+				}
+			}
+		}
+		columns = y;
+		rows = x;
 	}
 
 
@@ -106,5 +157,21 @@ public class PrimeGenerator {
 
 	public void setPrimes(boolean[] primes) {
 		this.primes = primes;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
 	}
 }
--- src/ui/View.java
+++ src/ui/View.java
@@ -1,10 +1,182 @@
 package ui;
 
-public class View {
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.Skin;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import model.PrimeGenerator;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+
+
+
+
+public class View extends Application{
+	
+	public static PrimeGenerator primeGenerator;
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		primeGenerator = new PrimeGenerator();
+		launch(args);
 	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		System.out.println(primeGenerator.getTop());
+		GridPane pane = new GridPane();
+		
+		 TextField tf1 = new TextField("");
+		 // this sets the number of spaces in the text fields
+
+		 Label l1 = new Label("Write a natural number here ");
+
+		 // these labels are set and used before the text fields as show below
+
+		 pane.add(l1, 0, 0);
+		 pane.add(tf1, 1, 0);
+		 
+		 
+		 Button FirstAlgorithm = new Button("First Algorithm");
+		 Button SecondAlgorithm = new Button("Second Algorithm");
+		 Button ThirdAlgorithm = new Button("Third Algorithm");
+		 
+		 pane.add(FirstAlgorithm, 0, 1);
+		 pane.add(SecondAlgorithm, 1, 1);
+		 pane.add(ThirdAlgorithm, 2, 1);
+		 
+		 if(primeGenerator.getPrimes()!=null) {
+			System.out.println("Entre");
+			 boolean[] primes = primeGenerator.getPrimes();
+			int columns = primeGenerator.getColumns();
+			int rows = primeGenerator.getRows();
+			int z=1;
+			for (int i = 0; i < rows; i++) {
+				for (int j = 0; j < columns; j++) {
+					Label label = new Label((z+1)+"");
+					if(primes[z]) {
+						label.setTextFill(Color.web("#ff0000"));
+					}
+					pane.add(label, (j), (i+2));
+					z++;
+				}
+			}
+		 }
+		 
+		 BorderPane borderPane = new BorderPane();
+		 borderPane.setCenter(pane);
+
+		 Scene scene = new Scene(borderPane, 500, 500);
+		 primaryStage.setTitle("Prime Generator");
+		 primaryStage.setScene(scene);
+		 primaryStage.show();
+
+		 
+		
+		 FirstAlgorithm.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				try {
+					
+					int top = Integer.parseInt(tf1.getText());
+					primeGenerator.setTop(top);
+					primeGenerator.firstAlgorithm();
+					primaryStage.close();
+					View v = new View();
+					v.start(new Stage());
+					
+					
+				}catch(NumberFormatException e) {
+					
+					Alert alert = new Alert(AlertType.ERROR);
+					alert.setTitle("Error");
+			        alert.setHeaderText("Results:");
+			        alert.setContentText("Insert a correct natural number");
+			        alert.showAndWait();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		 
+		 SecondAlgorithm.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				try {
+					
+					int top = Integer.parseInt(tf1.getText());
+					primeGenerator.setTop(top);
+					primeGenerator.secondAlgorithm();
+					primaryStage.close();
+					View v = new View();
+					v.start(new Stage());
+					
+				}catch(NumberFormatException e) {
+					
+					Alert alert = new Alert(AlertType.ERROR);
+					alert.setTitle("Error");
+			        alert.setHeaderText("Results:");
+			        alert.setContentText("Insert a correct natural number");
+			        alert.showAndWait();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		});
+		 
+		 ThirdAlgorithm.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				try {
+					
+					int top = Integer.parseInt(tf1.getText());
+					primeGenerator.setTop(top);
+					primeGenerator.thirdAlgorithm();
+					primaryStage.close();
+					View v = new View();
+					v.start(new Stage());
+				}catch(NumberFormatException e) {
+					
+					Alert alert = new Alert(AlertType.ERROR);
+					alert.setTitle("Error");
+			        alert.setHeaderText("Results:");
+			        alert.setContentText("Insert a correct natural number");
+			        alert.showAndWait();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		});	 	
+	}
 }
